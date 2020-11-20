0823469dcb0a855e89ed8ff10926152ea8397d38
Dmitry <gmyrak@gmail.com>
2020-11-20 21:36:27 +0200

    2

--- Program.cs
+++ Program.cs
@@ -33,7 +33,6 @@ namespace books
             using (SQLiteDataReader reader = command.ExecuteReader())
             {
                 int fieldCount = reader.FieldCount;
-
                 while (reader.Read())
                 {
                     for (int i = 0; i < fieldCount; i++)
@@ -44,18 +43,17 @@ namespace books
             }
         }
 
-        private void modify(String sql)
+        private int modify(String sql)
         {
             SQLiteCommand command = dbConnect.CreateCommand();
             command.CommandText = sql;
-            command.ExecuteNonQuery();
+            return command.ExecuteNonQuery();
         }
 
         public void showBooks(String title)
         {
             select($"SELECT * FROM books WHERE title LIKE '{title}%'");
         }
-
 
     }
 
@@ -66,10 +64,38 @@ namespace books
 
             BooksStorage bs = new BooksStorage(@"c:\Projects\books\database.db");
 
-            bs.showBooks("Т");
-            //bs.select($"SELECT * FROM books WHERE title LIKE '%'");
+            String prompt = "1 - SELECT, 2 - INSERT, 3 - UPDATE, 4 - DELETE";
 
-            Console.WriteLine("Hello!");
+
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                ConsoleKeyInfo key = Console.ReadKey(false);
+                
+                switch (key.KeyChar)
+                {
+                    case '1': 
+                        Console.WriteLine("first");
+                        break;
+
+                    case '2':
+                        Console.WriteLine("second");
+                        break;
+
+                    default:
+                        Console.WriteLine("Other");
+                        break;
+
+                }
+
+            }
+
+
+
+            //bs.showBooks("Т");
+            //bs.select($"SELECT * FROM books WHERE title LIKE '%'");
+                        
+            Console.WriteLine("Bye!");
         }
     }
 }
